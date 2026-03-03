21c785c73a85efa74fc91121a026d3af45f640c6
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2026-03-03 22:26:06 +0530

    ecommerce product model  03Mar2026

---
+++ digit_notes/web_projects/Ecommerce/Ecommerce/src/main/java/com/example/Ecommerce/Model/Product.java
@@ -0,0 +1,20 @@
+package com.example.Ecommerce.Model;
+
+public class Product {
+    private String prodId;
+    private String prodName;
+
+    private String category;
+    private String prodDescription;
+
+    private double price;
+    private double mrp;
+    private int discountPercentage;
+
+    private boolean inStock;
+    private int stockQuantity;
+    private float rating;
+    private int totalRating;
+
+
+}
